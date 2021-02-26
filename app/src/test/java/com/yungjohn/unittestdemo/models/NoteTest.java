2fb37c17b09211ad362a023271f19244586029bd
princejohnnie <61082393+princejohnnie@users.noreply.github.com>
2021-02-26 09:19:48 +0100

    Test class setup

---
+++ app/src/test/java/com/yungjohn/unittestdemo/models/NoteTest.java
@@ -0,0 +1,34 @@
+package com.yungjohn.unittestdemo.models;
+
+import org.junit.jupiter.api.Test;
+
+public class NoteTest{
+    /*
+    Compare two equal Notes
+     */
+
+    @Test
+    void isEqual() throws Exception {
+        // Arrange
+
+        // Act
+
+        //Assert
+    }
+
+    /*
+    Compare notes with 2 different ids
+     */
+
+    /*
+    Compare two notes with different timestamps
+     */
+
+    /*
+    Compare two notes with different title
+     */
+
+    /*
+    Compare two notes with different content
+     */
+}
